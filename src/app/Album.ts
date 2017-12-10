087a94dc15affea23da442d4f9badd0dc5c45f0e
sebabarre <sebabarre3@gmail.com>
2017-12-10 12:22:54 +0100

    WIP ajout bouton follow

---
+++ src/app/Album.ts
@@ -0,0 +1,11 @@
+export class Album {
+
+	constructor(
+		public id: number,
+		public name: string,
+		public apiId: string,
+		public apiUrl: string,
+		public thumb: string,
+		public year: number
+	) {}
+}
--- src/app/artist-search-form.component.ts
+++ src/app/artist-search-form.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Artist } from './Artist';
+import { Album } from './Album';
 import { HttpClient } from '@angular/common/http';
+import { HttpParams } from '@angular/common/http';
 
 @Component({
 	selector: 'artist-search-form',
@@ -11,6 +13,7 @@ export class ArtistSearchFormComponent  {
 	
   submitted = false;
   results: Artist[];
+  albums: Album[];
 
   constructor(private http: HttpClient) {}
 
@@ -25,7 +28,41 @@ export class ArtistSearchFormComponent  {
     });
   }
 
+  followThisOne(id) {
+    console.log(id);
+    this.http.get('http://localhost:8090/artist/add?idArtist=' + id)
+      .subscribe(
+        res => {
+          console.log(res);
+        },
+        err => {
+          console.log("Error occured");
+        }
+      );
+  }
+
+  getAll() {
+    this.http.get<Artist[]>('http://localhost:8090/artist/all').subscribe(data => {
+      // Read the result field from the JSON response.
+      this.results = data;
+      console.log(this.results)
+    });
+  }
+
+  onSubmitAlbum(artist) {
+    console.log(artist)
+    this.http.get<Album[]>('http://localhost:8090/artist/albums?idArtist=' + artist.apiId).subscribe(data => {
+      // Read the result field from the JSON response.
+      this.albums = data;
+    });
+  }
+
   // TODO: Remove this when we're done
   get diagnostic() { return JSON.stringify(this.model); }
+
+  toHttpParams(params) {
+    return Object.getOwnPropertyNames(params)
+                 .reduce((p, key) => p.set(key, params[key]), new HttpParams());
+}
 
 }
